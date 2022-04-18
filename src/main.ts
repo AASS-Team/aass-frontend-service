6ea3b69d5fbe71ad454813586bded460fbca7fbf
František Gič <mcfreddie777@gmail.com>
2022-04-18 20:09:58 +0200

    Setup TailwindCSS

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,45 @@
 import { createApp } from 'vue';
-import App from './App.vue';
 import router from './router';
 import store from './store';
 
-createApp(App).use(store).use(router).mount('#app');
+// Root styles
+import '@/assets/scss/app.scss';
+
+// Root component
+import App from './App.vue';
+
+// Local dependencies
+import FontAwesomeIcon from '@/icons';
+import filters from '@/filters';
+
+// Third party global dependencies
+import axios from 'axios';
+import { VTooltip } from 'floating-vue';
+
+// Create Vue app
+const app = createApp(App);
+
+// Vuex
+app.use(store);
+
+// Vue-router
+app.use(router);
+
+// Axios (to be addressed from script as this.$axios)
+const axiosInstance = axios.create({
+	baseURL: process.env.VUE_APP_API_URL
+});
+
+app.config.globalProperties.$axios = axiosInstance;
+
+// Font Awesome icons
+app.component('fa-icon', FontAwesomeIcon);
+
+// v-tooltip
+app.directive('tooltip', VTooltip);
+
+// Filters  (to be addressed from script as this.$filters)
+app.config.globalProperties.$filters = filters;
+
+// Finally, mount the app!
+app.mount('#app');
---
+++ tailwind.config.js
@@ -0,0 +1,65 @@
+module.exports = {
+	darkMode: false, // or 'media' or 'class'
+	content: ['./src/index.html', './src/**/*.vue', './src/mixins/*.mixin.ts'],
+	theme: {
+		extend: {
+			borderWidth: {
+				3: '3px'
+			},
+			colors: {
+				white: '#fdfdfd',
+				primary: {
+					100: '#ecd47f',
+					200: '#e1ba32',
+					300: '#ddb119',
+					400: '#f7bf00',
+					500: '#daa900',
+					700: '#c49800',
+					800: '#684F1D',
+					900: '#453411'
+				}
+			},
+			width: {
+				96: '24rem',
+				112: '28rem',
+				128: '32rem',
+				144: '36rem',
+				160: '40rem'
+			},
+			height: {
+				96: '24rem',
+				112: '28rem',
+				128: '32rem',
+				144: '36rem',
+				160: '40rem'
+			},
+			minHeight: {
+				10: '2.5rem'
+			},
+			maxHeight: {
+				'5/6': '83%'
+			},
+			fontSize: {
+				'7xl': '5rem',
+				'8xl': '6rem',
+				'9xl': '7rem',
+				'10xl': '8rem'
+			}
+		}
+	},
+	variants: {
+		visibility: ['group-hover'],
+		extend: {
+			borderWidth: ['first', 'last'],
+			margin: ['last']
+		},
+		padding: ['responsive', 'first', 'hover', 'focus', 'last'],
+		background: ['hover']
+	},
+	plugins: [],
+	safelist: [
+		{
+			pattern: /^w-/
+		}
+	]
+};
