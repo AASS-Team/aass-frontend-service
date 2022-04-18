dace2caf8e4c808177b53cdc5bc3104b346a8b87
František Gič <mcfreddie777@gmail.com>
2022-04-18 20:10:39 +0200

    Add basic filters

---
+++ src/filters/index.ts
@@ -0,0 +1,5 @@
+import toNumber from '@/filters/to-number.filter';
+import roundToTwoDecimals from '@/filters/round-to-two-decimals.filter';
+import stringify from '@/filters/stringify.filter';
+
+export default { toNumber, roundToTwoDecimals, stringify };
---
+++ src/filters/to-number.filter.ts
@@ -0,0 +1,3 @@
+export default function toNumber(value: string) {
+	return Number(value);
+}
