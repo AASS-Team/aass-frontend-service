a3c7b4f3c7e3543ccc5b4dc4736b5e93a0e6fac9
František Gič <mcfreddie777@gmail.com>
2022-04-18 20:08:42 +0200

    Rewrite main index.html

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,26 @@
-const { defineConfig } = require('@vue/cli-service');
-module.exports = defineConfig({
-	transpileDependencies: true,
-	lintOnSave: false
-});
+const path = require('path');
+
+module.exports = {
+	publicPath: '/',
+	outputDir: 'dist',
+	filenameHashing: process.env.NODE_ENV !== 'production',
+	productionSourceMap: false,
+	lintOnSave: false,
+	devServer: {
+		port: 8080
+	},
+	configureWebpack: {
+		resolve: {
+			alias: {
+				'@': path.join(__dirname, './src')
+			}
+		}
+	},
+	chainWebpack: config => {
+		config.plugin('html').tap(args => {
+			args[0].template = 'src/index.html';
+			args[0].favicon = 'src/assets/img/favicon.ico';
+			return args;
+		});
+	}
+};
