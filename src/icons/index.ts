f7a11e62d86ade9717740c70dd318bd9f2368bc1
František Gič <mcfreddie777@gmail.com>
2022-04-18 20:11:31 +0200

    Add font awesome icons

---
+++ src/icons/index.ts
@@ -0,0 +1,11 @@
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import {
+	faAngleDown,
+	faAngleRight,
+	faBars
+} from '@fortawesome/free-solid-svg-icons';
+
+library.add(faAngleDown, faAngleRight, faBars);
+
+export default FontAwesomeIcon;
