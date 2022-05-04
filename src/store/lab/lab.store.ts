066e332891502d5260e0408eaae83d802068582b
František Gič <mcfreddie777@gmail.com>
2022-05-04 14:12:13 +0200

    Replace mocked data with XHR calls

--- src/store/lab/lab.store.ts
+++ src/store/lab/lab.store.ts
@@ -5,6 +5,8 @@ import {
 	RootState
 } from '@/store/index.types';
 import { Actions, Getters, Lab, State } from '@/store/lab/lab.types';
+import axios from '@/services/axios';
+import { ResponseDataWrapper } from '@/types/response.type';
 
 const SET_LABS = 'set_labs';
 const SET_LAB = 'set_lab';
@@ -24,44 +26,9 @@ const getters: GetterTreeAdaptor<Getters, State, RootState> = {
 
 const actions: ActionTreeAdaptor<Actions, State, RootState> = {
 	fetchLabs({ commit, dispatch }) {
-		return new Promise<{ data: Lab[] }>(resolve => {
-			setTimeout(() => {
-				resolve({
-					data: [
-						{
-							id: '13ab4987-913e-4e4f-9aec-5f2c8cd1619e',
-							name: 'A211',
-							address: 'Radlinského 9, 812 37 Bratislava',
-							available: true
-						},
-						{
-							id: '2cfdcd14-4e80-4dc3-9c1f-39181802378d',
-							name: 'A212',
-							address: 'Radlinského 9, 812 37 Bratislava',
-							available: true
-						},
-						{
-							id: '431406e8-fbbd-4b54-a5cd-01173f681580',
-							name: 'A213',
-							address: 'Radlinského 9, 812 37 Bratislava',
-							available: false
-						},
-						{
-							id: '47edc17c-f2b1-4462-8285-964e22bd8949',
-							name: 'A214',
-							address: 'Radlinského 9, 812 37 Bratislava',
-							available: false
-						},
-						{
-							id: '7f62216a-9a82-420a-a6b6-0bb3a28685b6',
-							name: 'A215',
-							address: 'Radlinského 9, 812 37 Bratislava',
-							available: true
-						}
-					]
-				});
-			}, 750);
-		})
+		return axios
+			.get<ResponseDataWrapper<Lab[]>>('/api/labs/')
+			.then(response => response.data)
 			.then(response => {
 				commit(SET_LABS, response.data);
 			})
@@ -80,18 +47,9 @@ const actions: ActionTreeAdaptor<Actions, State, RootState> = {
 			});
 	},
 	fetchLab({ commit, dispatch }, id) {
-		return new Promise<{ data: Lab }>(resolve => {
-			setTimeout(() => {
-				resolve({
-					data: {
-						id: id,
-						name: 'A211',
-						address: 'Radlinského 9, 812 37 Bratislava',
-						available: true
-					}
-				});
-			}, 750);
-		})
+		return axios
+			.get<ResponseDataWrapper<Lab>>(`/api/labs/${id}`)
+			.then(response => response.data)
 			.then(response => {
 				commit(SET_LAB, response.data);
 			})
@@ -110,19 +68,12 @@ const actions: ActionTreeAdaptor<Actions, State, RootState> = {
 			});
 	},
 	saveLab({ commit, dispatch }, lab) {
-		return new Promise<{ data: Lab }>(resolve => {
-			setTimeout(() => {
-				resolve({
-					data: {
-						id: Math.random().toString(),
-						name: lab.name,
-						address: lab.address,
-						available: true
-					}
-				});
-			}, 750);
-		})
+		console.log('Lab: ', lab);
+		return axios
+			.post<ResponseDataWrapper<Lab>>(`/api/labs/`, lab)
+			.then(response => response.data)
 			.then(response => {
+				console.log('Response: ', response);
 				commit(ADD_LAB, response.data);
 			})
 			.catch(e => {
@@ -140,18 +91,12 @@ const actions: ActionTreeAdaptor<Actions, State, RootState> = {
 			});
 	},
 	updateLab({ commit, dispatch }, { id, lab }) {
-		return new Promise<{ data: Lab }>(resolve => {
-			setTimeout(() => {
-				resolve({
-					data: {
-						id: id,
-						name: lab.name,
-						address: lab.address,
-						available: true
-					}
-				});
-			}, 750);
-		})
+		return axios
+			.put<ResponseDataWrapper<Lab>>(`/api/labs/${id}`, {
+				name: lab.name,
+				address: lab.address
+			})
+			.then(response => response.data)
 			.then(response => {
 				commit(UPDATE_LAB, { id, lab: response.data });
 			})
@@ -170,11 +115,8 @@ const actions: ActionTreeAdaptor<Actions, State, RootState> = {
 			});
 	},
 	deleteLab({ commit, dispatch }, id) {
-		return new Promise(resolve => {
-			setTimeout(() => {
-				resolve(null);
-			}, 750);
-		})
+		return axios
+			.delete<ResponseDataWrapper<Lab>>(`/api/labs/${id}`)
 			.then(() => {
 				commit(DELETE_LAB, id);
 			})
